826eb71a259728cf4dc9e2eac0be22b1fe3af0c1
DiegoCruch <diegojosuerodriguez@gmail.com>
2020-05-11 18:08:52 -0600

    ejercicios ya resueltos

---
+++ ejercicio1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int a, b, c;
+double p;
+
+
+cout<<"ingrese el primer numero: ";
+cin>> a;
+
+cout<<"ingrese el segundo numero: ";
+cin>> b;
+
+cout<<"ingrese el tercer numero: ";
+cin>> c;
+
+p= (a+b+c)/3.0;
+cout<<"El promedio de es: "<<p;
+
+return 0;
+}
---
+++ ejercicio2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+main(){
+    float r, a, p;
+    double pi;
+
+    cout<<"ingrese el radio del circulo:";
+    cin>>r;
+
+    pi= 4.0*atan(1.0);
+    a=pi*pow(r,2);
+    cout<<"perimetro es :" <<p<endl;
+    return 0;
+
+}
+
+}
