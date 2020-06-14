e15295f0313742aeb5cac0f08cccbc733abd4804
DiegoCruch <diegojosuerodriguez@gmail.com>
2020-06-13 21:47:25 -0600

    cambios

--- examen3/eje1.cpp
+++ examen3/eje1.cpp
@@ -3,29 +3,35 @@
 #include <string>
 using namespace std;
 
+int horasfun(){
+    int h;
+    cout<<"Ingrese las horas trabajadas: "<< endl;
+    cin>> h;
+    return h;
+}
+
+int horasex(){
+    int e;
+    cout<<"Ingrese las horas extras: "<<endl;
+    cin>> e;
+    return e;
+}
 
 int main() {
-    int HT, HE;
+    int HT = horasfun();
+    int  HE = horasex();
     float SH, SM, SE, desc, SD, ST;
-   
-
+    int HT = horasfun();
 
     cout << "Ingrese su salario por hora"<< endl;
     cin>> SH;
    
     cout << "Ingrese su salario por horas extra"<< endl;
     cin>> SE;
- 
-    cout << "Ingrese las horas trabajadas "<<endl;
-    cin>> HT;
-  
-    cout << "Ingrese las horas extras "<<endl;
-    cin>> HE;
-
     SM= (SH*HT)+(HE*SE);
     
     cout << "El salario mensual es: "<< SM<< endl;
-    if(SM<500){
+    if(SM>500){
         desc = (SM*0.10) + (SM*0.04) + (SM*0.0625);
         ST= (SM-desc);
     }else{
